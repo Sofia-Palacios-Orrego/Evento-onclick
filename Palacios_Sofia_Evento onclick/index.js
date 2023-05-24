70bb28de26418aa7a69560888587f9252feb189c
Sofia-Palacios-Orrego <133055759+Sofia-Palacios-Orrego@users.noreply.github.com>
2023-05-24 12:06:37 -0400

    Add files via upload

---
+++ Palacios_Sofia_Evento onclick/index.js
@@ -0,0 +1,12 @@
+function likeNotif() {
+    alert("Ninja was liked");
+}
+
+
+function removeBtn (element) {
+        element.remove();
+}
+
+function loginNout(element){
+    element.innerText="Logout";
+}
